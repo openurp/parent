2565add71e871ec185ba9f9b3c2f205176cd1bf0
段体华 <duantihua@gmail.com>
2021-10-22 18:19:21 +0800

    update ems 4.1.26

--- src/main/scala/org/openurp/parent/Dependencies.scala
+++ src/main/scala/org/openurp/parent/Dependencies.scala
@@ -32,7 +32,7 @@ object Dependencies extends sbt.AutoPlugin {
   val beangle_security_ver="4.2.31"
   val beangle_ids_ver="0.2.23"
   val beangle_doc_ver="0.0.8"
-  val beangle_ems_ver="4.1.25"
+  val beangle_ems_ver="4.1.26"
 
   val beangle_commons_core = "org.beangle.commons" %% "beangle-commons-core" % beangle_commons_ver
   val beangle_commons_file = "org.beangle.commons" %% "beangle-commons-file" % beangle_commons_ver
