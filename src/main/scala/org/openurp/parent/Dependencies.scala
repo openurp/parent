4c016520156c8de867c3b251b63eec1199ccba2d
段体华 <duantihua@gmail.com>
2022-04-17 10:07:54 +0800

    update to data 5.4.2

--- src/main/scala/org/openurp/parent/Dependencies.scala
+++ src/main/scala/org/openurp/parent/Dependencies.scala
@@ -20,14 +20,14 @@ package org.openurp.parent
 import sbt._
 
 object Dependencies {
-  val beangle_commons_ver = "5.2.13"
-  val beangle_data_ver = "5.4.0"
+  val beangle_commons_ver = "5.2.15"
+  val beangle_data_ver = "5.4.2"
   val beangle_cdi_ver = "0.3.5"
   val beangle_web_ver = "0.0.6"
   val beangle_serializer_ver= "0.0.23"
   val beangle_cache_ver= "0.0.26"
   val beangle_template_ver ="0.0.37"
-  val beangle_webmvc_ver="0.4.9"
+  val beangle_webmvc_ver="0.4.10"
   val beangle_security_ver="4.2.33"
   val beangle_ids_ver="0.2.26"
   val beangle_doc_ver="0.0.10"
