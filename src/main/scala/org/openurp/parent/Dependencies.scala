083a7628ae4a536a7ce42055999fc594c04653fa
段体华 <duantihua@gmail.com>
2022-08-12 04:04:11 +0800

    update to webmvc 0.5.4

--- src/main/scala/org/openurp/parent/Dependencies.scala
+++ src/main/scala/org/openurp/parent/Dependencies.scala
@@ -21,13 +21,13 @@ import sbt._
 
 object Dependencies {
   val beangle_commons_ver = "5.3.0"
-  val beangle_data_ver = "5.4.6"
+  val beangle_data_ver = "5.4.7"
   val beangle_cdi_ver = "0.3.7"
-  val beangle_web_ver = "0.1.0"
+  val beangle_web_ver = "0.1.1"
   val beangle_serializer_ver= "0.0.24"
   val beangle_cache_ver= "0.0.27"
   val beangle_template_ver ="0.0.38"
-  val beangle_webmvc_ver="0.5.3"
+  val beangle_webmvc_ver="0.5.4"
   val beangle_security_ver="4.2.35"
   val beangle_ids_ver="0.2.28"
   val beangle_doc_ver="0.0.11"
