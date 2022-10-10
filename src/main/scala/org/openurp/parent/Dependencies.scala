562900bf53100d91696618f2f5b2104a4d07920e
段体华 <duantihua@gmail.com>
2022-10-10 11:32:49 +0800

    update commons 5.4.1

--- src/main/scala/org/openurp/parent/Dependencies.scala
+++ src/main/scala/org/openurp/parent/Dependencies.scala
@@ -20,18 +20,18 @@ package org.openurp.parent
 import sbt._
 
 object Dependencies {
-  val beangle_commons_ver = "5.4.0"
-  val beangle_data_ver = "5.5.2"
-  val beangle_cdi_ver = "0.4.0"
-  val beangle_web_ver = "0.3.1"
-  val beangle_serializer_ver= "0.1.0"
-  val beangle_cache_ver= "0.1.0"
-  val beangle_template_ver ="0.1.0"
-  val beangle_webmvc_ver="0.6.2"
-  val beangle_security_ver="4.3.1"
-  val beangle_ids_ver="0.3.2"
-  val beangle_doc_ver="0.1.0"
-  val beangle_ems_ver="4.5.4"
+  val beangle_commons_ver = "5.4.1"
+  val beangle_data_ver = "5.5.4"
+  val beangle_cdi_ver = "0.4.1"
+  val beangle_web_ver = "0.3.2"
+  val beangle_serializer_ver= "0.1.1"
+  val beangle_cache_ver= "0.1.1"
+  val beangle_template_ver ="0.1.1"
+  val beangle_webmvc_ver="0.6.3"
+  val beangle_security_ver="4.3.2"
+  val beangle_ids_ver="0.3.3"
+  val beangle_doc_ver="0.1.1"
+  val beangle_ems_ver="4.5.5"
 
   val beangle_commons_core = "org.beangle.commons" %% "beangle-commons-core" % beangle_commons_ver
   val beangle_commons_file = "org.beangle.commons" %% "beangle-commons-file" % beangle_commons_ver
