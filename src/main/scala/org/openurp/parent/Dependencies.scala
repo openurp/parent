7b8fbee6cfa791e7f3ffc87c28ebbea53d8a1012
段体华 <duantihua@gmail.com>
2022-10-16 16:55:36 +0800

    update to 0.5.29

--- src/main/scala/org/openurp/parent/Dependencies.scala
+++ src/main/scala/org/openurp/parent/Dependencies.scala
@@ -23,15 +23,15 @@ object Dependencies {
   val beangle_commons_ver = "5.4.1"
   val beangle_data_ver = "5.5.4"
   val beangle_cdi_ver = "0.4.1"
-  val beangle_web_ver = "0.3.2"
+  val beangle_web_ver = "0.3.3"
   val beangle_serializer_ver= "0.1.1"
   val beangle_cache_ver= "0.1.1"
   val beangle_template_ver ="0.1.1"
-  val beangle_webmvc_ver="0.6.3"
+  val beangle_webmvc_ver="0.6.4"
   val beangle_security_ver="4.3.2"
   val beangle_ids_ver="0.3.3"
   val beangle_doc_ver="0.1.1"
-  val beangle_ems_ver="4.5.5"
+  val beangle_ems_ver="4.5.7"
 
   val beangle_commons_core = "org.beangle.commons" %% "beangle-commons-core" % beangle_commons_ver
   val beangle_commons_file = "org.beangle.commons" %% "beangle-commons-file" % beangle_commons_ver
