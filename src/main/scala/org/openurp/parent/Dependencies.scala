74a8f228bdfd8951b2a52805b3e9d461dea91018
段体华 <duantihua@gmail.com>
2024-03-12 07:48:03 +0800

    update to ems 4.8.10

--- src/main/scala/org/openurp/parent/Dependencies.scala
+++ src/main/scala/org/openurp/parent/Dependencies.scala
@@ -28,9 +28,9 @@ object Dependencies {
   val beangle_cache_ver= "0.1.7"
   val beangle_template_ver ="0.1.11"
   val beangle_webmvc_ver="0.9.23"
-  val beangle_security_ver="4.3.17"
+  val beangle_security_ver="4.3.18"
   val beangle_ids_ver="0.3.15"
-  val beangle_ems_ver="4.8.9"
+  val beangle_ems_ver="4.8.10"
   val beangle_doc_ver="0.3.2"
 
   val beangle_commons_core = "org.beangle.commons" %% "beangle-commons-core" % beangle_commons_ver
