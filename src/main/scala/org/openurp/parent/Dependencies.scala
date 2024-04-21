ce64fedcd28378e7d5b07aae0c6872705c0b7777
段体华 <duantihua@gmail.com>
2024-04-21 12:40:59 +0800

    update to ems 4.9.0

--- src/main/scala/org/openurp/parent/Dependencies.scala
+++ src/main/scala/org/openurp/parent/Dependencies.scala
@@ -20,19 +20,20 @@ package org.openurp.parent
 import sbt._
 
 object Dependencies {
-  val beangle_commons_ver = "5.6.15"
-  val beangle_jdbc_ver = "1.0.0"
-  val beangle_data_ver = "5.8.9"
-  val beangle_cdi_ver = "0.6.5"
-  val beangle_web_ver = "0.4.11"
-  val beangle_serializer_ver= "0.1.9"
-  val beangle_cache_ver= "0.1.8"
-  val beangle_template_ver ="0.1.14"
-  val beangle_webmvc_ver="0.9.26"
-  val beangle_security_ver="4.3.19"
-  val beangle_ids_ver="0.3.16"
-  val beangle_ems_ver="4.8.14"
-  val beangle_doc_ver="0.3.3"
+  val beangle_commons_ver = "5.6.16"
+  val beangle_jdbc_ver = "1.0.1"
+  val beangle_data_ver = "5.8.10"
+  val beangle_cdi_ver = "0.6.6"
+  val beangle_web_ver = "0.4.12"
+  val beangle_serializer_ver= "0.1.10"
+  val beangle_cache_ver= "0.1.9"
+  val beangle_template_ver ="0.1.15"
+  val beangle_webmvc_ver="0.9.27"
+  val beangle_security_ver="4.3.20"
+  val beangle_ids_ver="0.3.17"
+  val beangle_event_ver="0.0.5"
+  val beangle_doc_ver="0.3.4"
+  val beangle_ems_ver="4.9.0"
 
   val beangle_commons = "org.beangle.commons" % "beangle-commons" % beangle_commons_ver
 
@@ -48,7 +49,9 @@ object Dependencies {
   val beangle_serializer = "org.beangle.serializer" % "beangle-serializer" % beangle_serializer_ver
 
   val beangle_security = "org.beangle.security" % "beangle-security" % beangle_security_ver
-  val beangle_ids_cas = "org.beangle.ids" % "beangle-ids" % beangle_ids_ver
+  val beangle_ids = "org.beangle.ids" % "beangle-ids" % beangle_ids_ver
+
+  val beangle_event = "org.beangle.event" % "beangle-event" % beangle_event_ver
 
   val beangle_ems_app = "org.beangle.ems" % "beangle-ems-app" % beangle_ems_ver
   val beangle_doc_pdf = "org.beangle.doc" % "beangle-doc-pdf" % beangle_doc_ver
@@ -57,12 +60,12 @@ object Dependencies {
   val beangle_doc_transfer = "org.beangle.doc" % "beangle-doc-transfer" % beangle_doc_ver
 
   var freemarker = "org.freemarker" % "freemarker" % "2.3.32"
-  val spring_beans = "org.springframework" % "spring-beans" % "6.1.4"
-  val spring_context = "org.springframework" % "spring-context" % "6.1.4"
-  val spring_tx = "org.springframework" % "spring-tx" % "6.1.4"
-  val spring_aop = "org.springframework" % "spring-aop" % "6.1.4"
-  val spring_jdbc = "org.springframework" % "spring-jdbc" % "6.1.4"
-  val protobuf = "com.google.protobuf" % "protobuf-java" % "3.25.1"
+  val spring_beans = "org.springframework" % "spring-beans" % "6.1.6"
+  val spring_context = "org.springframework" % "spring-context" % "6.1.6"
+  val spring_tx = "org.springframework" % "spring-tx" % "6.1.6"
+  val spring_aop = "org.springframework" % "spring-aop" % "6.1.6"
+  val spring_jdbc = "org.springframework" % "spring-jdbc" % "6.1.6"
+  val protobuf = "com.google.protobuf" % "protobuf-java" % "3.25.3"
 
   val scalatest = "org.scalatest" %% "scalatest" % "3.2.18" % "test"
   val servletapi = "jakarta.servlet" % "jakarta.servlet-api" % "6.0.0"
@@ -71,6 +74,6 @@ object Dependencies {
   val hibernate_core = "org.beangle.hibernate" % "beangle-hibernate-core" % "6.4.4.Final"
   val hibernate_jcache = "org.hibernate.orm" % "hibernate-jcache" % "6.4.4.Final" exclude("org.hibernate.orm", "hibernate-core")
   val gson ="com.google.code.gson" % "gson" % "2.10.1"
-  val logback_classic = "ch.qos.logback" % "logback-classic" % "1.5.0"
-  val logback_core = "ch.qos.logback" % "logback-core" % "1.5.0"
+  val logback_classic = "ch.qos.logback" % "logback-classic" % "1.5.6"
+  val logback_core = "ch.qos.logback" % "logback-core" % "1.5.6"
 }
