8d82add9dd2948857ae56df74d4ff44ccbdc3db0
段体华 <duantihua@gmail.com>
2025-06-18 21:28:41 +0800

    update to doc 0.4.14

--- src/main/scala/org/openurp/parent/Dependencies.scala
+++ src/main/scala/org/openurp/parent/Dependencies.scala
@@ -20,7 +20,7 @@ package org.openurp.parent
 import sbt.*
 
 object Dependencies {
-  val beangle_commons = "org.beangle.commons" % "beangle-commons" % "5.6.28"
+  val beangle_commons = "org.beangle.commons" % "beangle-commons" % "5.6.29"
 
   val beangle_jdbc = "org.beangle.jdbc" % "beangle-jdbc" % "1.0.11"
   val beangle_model = "org.beangle.data" % "beangle-model" % "5.8.24"
@@ -34,17 +34,17 @@ object Dependencies {
   val beangle_bui_bootstrap = "org.beangle.bui" % "beangle-bui-bootstrap" % "0.0.6"
   val beangle_serializer = "org.beangle.serializer" % "beangle-serializer" % "0.1.18"
 
-  val beangle_security = "org.beangle.security" % "beangle-security" % "4.3.28"
+  val beangle_security = "org.beangle.security" % "beangle-security" % "4.3.29"
   val beangle_ids = "org.beangle.ids" % "beangle-ids" % "0.3.24"
 
   val beangle_event = "org.beangle.event" % "beangle-event" % "0.1.0"
 
-  val beangle_ems_app = "org.beangle.ems" % "beangle-ems-app" % "4.11.2"
-  val beangle_doc_pdf = "org.beangle.doc" % "beangle-doc-pdf" % "0.4.13"
-  val beangle_doc_docx = "org.beangle.doc" % "beangle-doc-docx" % "0.4.13"
-  val beangle_doc_excel = "org.beangle.doc" % "beangle-doc-excel" % "0.4.13"
-  val beangle_doc_transfer = "org.beangle.doc" % "beangle-doc-transfer" % "0.4.13"
-  val beangle_doc_html = "org.beangle.doc" % "beangle-doc-html" % "0.4.13"
+  val beangle_ems_app = "org.beangle.ems" % "beangle-ems-app" % "4.11.3"
+  val beangle_doc_pdf = "org.beangle.doc" % "beangle-doc-pdf" % "0.4.14"
+  val beangle_doc_docx = "org.beangle.doc" % "beangle-doc-docx" % "0.4.14"
+  val beangle_doc_excel = "org.beangle.doc" % "beangle-doc-excel" % "0.4.14"
+  val beangle_doc_transfer = "org.beangle.doc" % "beangle-doc-transfer" % "0.4.14"
+  val beangle_doc_html = "org.beangle.doc" % "beangle-doc-html" % "0.4.14"
 
   val beangle_notify = "org.beangle.notify" % "beangle-notify" % "0.1.13"
 
