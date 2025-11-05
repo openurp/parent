82310aaf97aa05258bece423eb82fbd9d5ab068c
段体华 <duantihua@gmail.com>
2025-11-05 09:14:59 +0800

    update to scala 3.3.7

--- src/main/scala/org/openurp/parent/Settings.scala
+++ src/main/scala/org/openurp/parent/Settings.scala
@@ -30,7 +30,7 @@ object Settings extends sbt.AutoPlugin {
     organizationName := "The OpenURP Software",
     licenses += ("GNU Lesser General Public License version 3", url("http://www.gnu.org/licenses/lgpl-3.0.txt")),
     startYear := Some(2014),
-    scalaVersion := "3.3.6",
+    scalaVersion := "3.3.7",
     scalacOptions := Seq("-Xtarget:21", "-deprecation", "-feature"),
     javacOptions := Seq("--release", "21", "-encoding", "utf-8"),
     crossPaths := false,
