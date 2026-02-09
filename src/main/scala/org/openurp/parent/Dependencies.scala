cbc1f82321c28c782691b4370a22b590d786205b
段体华 <duantihua@gmail.com>
2026-02-09 12:59:50 +0800

    update to ems 4.15.6

--- src/main/scala/org/openurp/parent/Dependencies.scala
+++ src/main/scala/org/openurp/parent/Dependencies.scala
@@ -30,17 +30,17 @@ object Dependencies {
   val beangle_template = "org.beangle.template" % "beangle-template" % "0.2.5"
 
   val beangle_web = "org.beangle.web" % "beangle-web" % "0.7.3"
-  val beangle_webmvc = "org.beangle.webmvc" % "beangle-webmvc" % "0.13.2"
-  val beangle_bui_bootstrap = "org.beangle.bui" % "beangle-bui-bootstrap" % "0.1.3"
+  val beangle_webmvc = "org.beangle.webmvc" % "beangle-webmvc" % "0.13.3"
+  val beangle_bui_bootstrap = "org.beangle.bui" % "beangle-bui-bootstrap" % "0.1.4"
   val beangle_serializer = "org.beangle.serializer" % "beangle-serializer" % "0.1.24"
 
   val beangle_security = "org.beangle.security" % "beangle-security" % "4.4.6"
   val beangle_ids = "org.beangle.ids" % "beangle-ids" % "0.4.6"
 
   val beangle_event = "org.beangle.event" % "beangle-event" % "0.1.4"
-  val beangle_config = "org.beangle.config" % "beangle-config" % "1.1.2"
+  val beangle_config = "org.beangle.config" % "beangle-config" % "1.1.3"
 
-  val beangle_ems_app = "org.beangle.ems" % "beangle-ems-app" % "4.15.5"
+  val beangle_ems_app = "org.beangle.ems" % "beangle-ems-app" % "4.15.6"
   val beangle_doc_pdf = "org.beangle.doc" % "beangle-doc-pdf" % "0.5.2"
   val beangle_doc_docx = "org.beangle.doc" % "beangle-doc-docx" % "0.5.2"
   val beangle_doc_excel = "org.beangle.doc" % "beangle-doc-excel" % "0.5.2"
