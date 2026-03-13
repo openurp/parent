80ca60c3d5ac904972140caf06314c20e4b0b8b4
段体华 <duantihua@gmail.com>
2026-03-13 17:00:55 +0800

    update event 0.1.5

--- src/main/scala/org/openurp/parent/Dependencies.scala
+++ src/main/scala/org/openurp/parent/Dependencies.scala
@@ -38,10 +38,10 @@ object Dependencies {
   val beangle_security = "org.beangle.security" % "beangle-security" % "4.4.8"
   val beangle_ids = "org.beangle.ids" % "beangle-ids" % "0.4.8"
 
-  val beangle_event = "org.beangle.event" % "beangle-event" % "0.1.4"
+  val beangle_event = "org.beangle.event" % "beangle-event" % "0.1.5"
   val beangle_config = "org.beangle.config" % "beangle-config" % "1.1.7"
 
-  val beangle_ems_app = "org.beangle.ems" % "beangle-ems-app" % "4.16.4"
+  val beangle_ems_app = "org.beangle.ems" % "beangle-ems-app" % "4.16.5"
   val beangle_doc_pdf = "org.beangle.doc" % "beangle-doc-pdf" % "0.5.3"
   val beangle_doc_docx = "org.beangle.doc" % "beangle-doc-docx" % "0.5.3"
   val beangle_doc_excel = "org.beangle.doc" % "beangle-doc-excel" % "0.5.3"
