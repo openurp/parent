271fa768cffc6796e82893c28fdba088c5bebc98
段体华 <duantihua@gmail.com>
2026-03-23 13:42:48 +0800

    update to webmvc 0.14.2

--- src/main/scala/org/openurp/parent/Dependencies.scala
+++ src/main/scala/org/openurp/parent/Dependencies.scala
@@ -20,7 +20,7 @@ package org.openurp.parent
 import sbt.*
 
 object Dependencies {
-  val beangle_commons = "org.beangle.commons" % "beangle-commons" % "6.0.12"
+  val beangle_commons = "org.beangle.commons" % "beangle-commons" % "6.0.14"
 
   val beangle_jdbc = "org.beangle.jdbc" % "beangle-jdbc" % "1.1.8"
   val beangle_data_model = "org.beangle.data" % "beangle-data-model" % "5.12.1"
@@ -30,18 +30,18 @@ object Dependencies {
   val beangle_cache = "org.beangle.cache" % "beangle-cache" % "0.1.19"
   val beangle_template = "org.beangle.template" % "beangle-template" % "0.2.5"
 
-  val beangle_web = "org.beangle.web" % "beangle-web" % "0.7.4"
-  val beangle_webmvc = "org.beangle.webmvc" % "beangle-webmvc" % "0.14.0"
+  val beangle_web = "org.beangle.web" % "beangle-web" % "0.7.5"
+  val beangle_webmvc = "org.beangle.webmvc" % "beangle-webmvc" % "0.14.2"
   val beangle_bui_bootstrap = "org.beangle.bui" % "beangle-bui-bootstrap" % "0.1.5"
-  val beangle_serializer = "org.beangle.serializer" % "beangle-serializer" % "0.1.24"
+  val beangle_serializer = "org.beangle.serializer" % "beangle-serializer" % "0.1.25"
 
-  val beangle_security = "org.beangle.security" % "beangle-security" % "4.4.8"
-  val beangle_ids = "org.beangle.ids" % "beangle-ids" % "0.4.8"
+  val beangle_security = "org.beangle.security" % "beangle-security" % "4.4.10"
+  val beangle_ids = "org.beangle.ids" % "beangle-ids" % "0.4.10"
 
-  val beangle_event = "org.beangle.event" % "beangle-event" % "0.1.5"
+  val beangle_event = "org.beangle.event" % "beangle-event" % "0.1.6"
   val beangle_config = "org.beangle.config" % "beangle-config" % "1.1.7"
 
-  val beangle_ems_app = "org.beangle.ems" % "beangle-ems-app" % "4.16.5"
+  val beangle_ems_app = "org.beangle.ems" % "beangle-ems-app" % "4.17.1"
   val beangle_doc_pdf = "org.beangle.doc" % "beangle-doc-pdf" % "0.5.3"
   val beangle_doc_docx = "org.beangle.doc" % "beangle-doc-docx" % "0.5.3"
   val beangle_doc_excel = "org.beangle.doc" % "beangle-doc-excel" % "0.5.3"
@@ -49,11 +49,11 @@ object Dependencies {
   val beangle_transfer = "org.beangle.transfer" % "beangle-transfer" % "0.0.6"
 
   val beangle_notify = "org.beangle.notify" % "beangle-notify" % "0.1.20"
-  val beangle_she = "org.beangle.she" % "beangle-she" % "0.0.4"
-  val beangle_cron = "org.beangle.cron" % "beangle-cron" % "0.0.3"
+  val beangle_she = "org.beangle.she" % "beangle-she" % "0.0.6"
+  val beangle_cron = "org.beangle.cron" % "beangle-cron" % "0.0.4"
 
   var freemarker = "org.freemarker" % "freemarker" % "2.3.34"
-  val protobuf = "com.google.protobuf" % "protobuf-java" % "4.34.0"
+  val protobuf = "com.google.protobuf" % "protobuf-java" % "4.34.1"
 
   val scalatest = "org.scalatest" %% "scalatest" % "3.2.19" % "test"
   val servletapi = "jakarta.servlet" % "jakarta.servlet-api" % "6.1.0"
