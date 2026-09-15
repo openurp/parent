edd8a4c927a05fa983e71783271cbc2ad4e94e4e
chaostone <duantihua@gmail.com>
2026-09-15 12:27:50 +0800

    update to ems 4.20.15

--- src/main/scala/org/openurp/parent/Dependencies.scala
+++ src/main/scala/org/openurp/parent/Dependencies.scala
@@ -42,7 +42,7 @@ object Dependencies {
   val beangle_event = "org.beangle.event" % "beangle-event" % "0.1.11"
   val beangle_config = "org.beangle.config" % "beangle-config" % "1.1.11"
 
-  val beangle_ems_app = "org.beangle.ems" % "beangle-ems-app" % "4.20.14"
+  val beangle_ems_app = "org.beangle.ems" % "beangle-ems-app" % "4.20.15"
   val beangle_doc_pdf = "org.beangle.doc" % "beangle-doc-pdf" % "0.5.13"
   val beangle_doc_docx = "org.beangle.doc" % "beangle-doc-docx" % "0.5.13"
   val beangle_doc_excel = "org.beangle.doc" % "beangle-doc-excel" % "0.5.13"
