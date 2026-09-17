d375f8c10d7fcdd6d6c821957d17eee3c602f6e2
chaostone <duantihua@gmail.com>
2026-09-17 14:58:14 +0800

    update to commons 6.3.6

--- src/main/scala/org/openurp/parent/Dependencies.scala
+++ src/main/scala/org/openurp/parent/Dependencies.scala
@@ -20,34 +20,34 @@ package org.openurp.parent
 import sbt.*
 
 object Dependencies {
-  val beangle_commons = "org.beangle.commons" % "beangle-commons" % "6.3.5"
+  val beangle_commons = "org.beangle.commons" % "beangle-commons" % "6.3.6"
 
-  val beangle_jdbc = "org.beangle.jdbc" % "beangle-jdbc" % "1.1.17"
+  val beangle_jdbc = "org.beangle.jdbc" % "beangle-jdbc" % "1.1.18"
   val beangle_data_model = "org.beangle.data" % "beangle-data-model" % "5.12.12"
   val beangle_data_hibernate = "org.beangle.data" % "beangle-data-hibernate" % "5.12.12"
   val beangle_cdi = "org.beangle.cdi" % "beangle-cdi" % "0.10.7"
 
   val beangle_cache = "org.beangle.cache" % "beangle-cache" % "0.1.22"
-  val beangle_template = "org.beangle.template" % "beangle-template" % "0.2.12"
+  val beangle_template = "org.beangle.template" % "beangle-template" % "0.2.13"
 
-  val beangle_web = "org.beangle.web" % "beangle-web" % "0.7.10"
-  val beangle_webmvc = "org.beangle.webmvc" % "beangle-webmvc" % "0.15.3"
+  val beangle_web = "org.beangle.web" % "beangle-web" % "0.7.11"
+  val beangle_webmvc = "org.beangle.webmvc" % "beangle-webmvc" % "0.15.4"
   val beangle_bui_bootstrap = "org.beangle.bui" % "beangle-bui-bootstrap" % "0.9.5"
   val beangle_bui_tag = "org.beangle.bui" % "beangle-bui-tag" % "0.9.5"
   val beangle_serializer = "org.beangle.serializer" % "beangle-serializer" % "0.1.28"
 
-  val beangle_security = "org.beangle.security" % "beangle-security" % "4.5.2"
-  val beangle_ids = "org.beangle.ids" % "beangle-ids" % "0.4.21"
+  val beangle_security = "org.beangle.security" % "beangle-security" % "4.5.4"
+  val beangle_ids = "org.beangle.ids" % "beangle-ids" % "0.4.22"
 
   val beangle_event = "org.beangle.event" % "beangle-event" % "0.1.11"
   val beangle_config = "org.beangle.config" % "beangle-config" % "1.1.11"
 
   val beangle_ems_app = "org.beangle.ems" % "beangle-ems-app" % "4.20.15"
-  val beangle_doc_pdf = "org.beangle.doc" % "beangle-doc-pdf" % "0.5.13"
-  val beangle_doc_docx = "org.beangle.doc" % "beangle-doc-docx" % "0.5.13"
-  val beangle_doc_excel = "org.beangle.doc" % "beangle-doc-excel" % "0.5.13"
-  val beangle_doc_html = "org.beangle.doc" % "beangle-doc-html" % "0.5.13"
-  val beangle_transfer = "org.beangle.transfer" % "beangle-transfer" % "0.0.8"
+  val beangle_doc_pdf = "org.beangle.doc" % "beangle-doc-pdf" % "0.5.14"
+  val beangle_doc_docx = "org.beangle.doc" % "beangle-doc-docx" % "0.5.14"
+  val beangle_doc_excel = "org.beangle.doc" % "beangle-doc-excel" % "0.5.14"
+  val beangle_doc_html = "org.beangle.doc" % "beangle-doc-html" % "0.5.14"
+  val beangle_transfer = "org.beangle.transfer" % "beangle-transfer" % "0.0.9"
 
   val beangle_notify = "org.beangle.notify" % "beangle-notify" % "0.1.27"
   val beangle_she = "org.beangle.she" % "beangle-she" % "0.0.18"
